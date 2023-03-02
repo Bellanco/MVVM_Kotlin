8c495f8d4c3330dfe0522708fa4ae6b166afd64e
Bellanco <bellanco3@gmail.com>
2023-03-02 23:17:51 +0100

    Change package name

--- app/src/androidTest/java/com/deromang/rick/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/deromang/rick/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.rick", appContext.packageName)
+        assertEquals("com.deromang.rick", appContext.packageName)
     }
 }
